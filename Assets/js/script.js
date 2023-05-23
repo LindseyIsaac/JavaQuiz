559265433608776509f2e7a02967e6d14d38ba19
LindseyIsaac <isaaclindsey89@gmail.com>
2023-05-23 18:34:41 -0500

    stuff

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,52 +1,231 @@
 var mainEl = document.getElementById('main');
-var startBtn = document.querySelector(".start-button");
-var timeEl = document.getElementById('countdown');
+var startBtn = document.querySelector("#start");
+var timeEl = document.getElementById('countdown'); 
+var initEl = document.getElementById("init");
+var qArea = document.getElementById("Q-area");
+var aArea = document.getElementById("A-area");
+var secondsLeft = 90;
+var a1Btn = document.getElementById("a1");
+var a2Btn = document.getElementById("a2");
+var a3Btn = document.getElementById("a3");
+var a4Btn = document.getElementById("a4");
+var form = document.getElementById("form");
+var aBtn = document.querySelectorAll(".A-btn");
+var feedback = document.getElementById("feedback");
+var feedbackExpiration;
+var message = document.getElementById("message");
+var initials = document.getElementById("initials");
+var inputField = document.getElementById("input");
+var submitBtn = document.getElementById("submit");
+var scoresArea = document.getElementById("scores-area");
+var scoreList = document.getElementById("scorelist");
+var reloadBtn = document.getElementById("reload");
 
-var questions 
-var answers
-var highScore
+
+// object full of objects for Qs and corresponding A arrays
+
+var quizChunks = [
+  {question: "What are you wearing",
+  ans1: "pizza",
+  ans2: "rats",
+  ans3: "skin suit",
+  ans4: "music",
+  correct: "skin suit"},
+
+  {question: "why are we here on earth",
+  ans1: "pizza",
+  ans2: "rats",
+  ans3: "boogers",
+  ans4: "to fart around",
+  correct: "to fart around"},
+
+  {question: "what is food",
+  ans1: "pizza",
+  ans2: "rats",
+  ans3: "food",
+  ans4: "all of the above",
+  correct: "all of the above"},
+
+  {question: "who deserves kindness",
+  ans1: "steve",
+  ans2: "everyone but steve",
+  ans3: "hugh grant",
+  ans4: "hugh grant",
+  correct: "everyone but steve"},
+
+  {question: "why am i sad",
+  ans1: "because of the horrors",
+  ans2: "pizza",
+  ans3: "rats", 
+  ans4: "hugh grant",
+  correct: "because of the horrors"},
+];
+
+var current = 0;
+var Q = quizChunks[current].question;
+var A1 = quizChunks[current].ans1;
+var A2 = quizChunks[current].ans2;
+var A3 = quizChunks[current].ans3;
+var A4 = quizChunks[current].ans4;
+
+showInit();
+
+
+function showInit(event) {
+  initEl.classList.toggle("hide");
+}
+
+var timerInterval;
+
+function setTime() {
+  timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+
+  }, 1000);
+}
+
+
+startBtn.addEventListener("click", function() {
+    setTime();
+    showQuiz();
+});
+
+
+function showQuiz() {
+  timeEl.classList.toggle("hide");
+  timeEl.textContent = secondsLeft;
+  initEl.classList.toggle( 'hide' );
+  qArea.classList.toggle( 'hide' );
+  aArea.classList.toggle( 'hide' );
+  qArea.innerHTML = Q;
+  a1Btn.innerHTML = A1;
+  a2Btn.innerHTML = A2;
+  a3Btn.innerHTML = A3;
+  a4Btn.innerHTML = A4;
+}
 
 
 
+aArea.addEventListener("click", check);
 
 
-
-
-
-// Timer that counts down from 20
-// function countdown() {
-//     var timeLeft = 20;
+function check(event) {
+  var right = quizChunks[current].correct;
   
-//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     var timeInterval = setInterval(function () {
-//       // As long as the `timeLeft` is greater than 1
-//       if (timeLeft > 1) {
-//         // Set the `textContent` of `timerEl` to show the remaining seconds
-//         timerEl.textContent = timeLeft + ' seconds remaining';
-//         // Decrement `timeLeft` by 1
-//         timeLeft--;
-//       } else if (timeLeft === 1) {
-//         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//         timerEl.textContent = timeLeft + ' second remaining';
-//         timeLeft--;
-//       } else {
-//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//         timerEl.textContent = '';
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(timeInterval);
-//         // Call the `displayMessage()` function
-//         displayMessage();
-//       }
-//     }, 20000);
-//   }
+  if (event.target.textContent == right) {
+    next();
+    feedback.innerHTML = "good doggy";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+  }
+  else {
+    punish(10);
+    next();
+    feedback.innerHTML = "how dare you";
+    feedbackExpiration = setTimeout(function () {
+      feedback.innerHTML = "";
+    }, 1000);
+    stopAtZero();
+    timeEl.textContent = secondsLeft;
+  };
+}
+
+function stopAtZero() {
+  if (secondsLeft <= 0) {
+    secondsLeft = 0;
+  endQuiz();
+}}
+
+function punish(seconds) {
+  secondsLeft -= seconds;
+}
+
+function hideFeedback() {
+  clearTimeout(feedbackExpiration);
+}
+
+
+function next() {
+  current++;
+  hideFeedback();
+
+  if (current < 5) {
+    var Q = quizChunks[current].question;
+    var A1 = quizChunks[current].ans1;
+    var A2 = quizChunks[current].ans2;
+    var A3 = quizChunks[current].ans3;
+    var A4 = quizChunks[current].ans4;
+ 
+    qArea.innerHTML = Q;
+    a1Btn.innerHTML = A1;
+    a2Btn.innerHTML = A2;
+    a3Btn.innerHTML = A3;
+    a4Btn.innerHTML = A4;
+  }
+
+  else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  qArea.classList.toggle("hide");
+  aArea.classList.toggle("hide");
+  form.classList.toggle("hide");
+  timeEl.classList.toggle("hide");
+  message.textContent = 
+  "I'm so proud of you just for existing. Your score is " + secondsLeft + ".";
+  initials.textContent = 
+  "put your initials here or whatever you feel like"
+}
+
+submitBtn.addEventListener("click", record);
+
+
+function record(event) {
+  event.preventDefault();
+  var input = inputField.value.trim();
+  var score = secondsLeft;
+  var game = {
+    player: input,
+    number: score,
+  };
+  var storedGames = JSON.parse(localStorage.getItem("storedgames"))||[];
+  storedGames.push(game);
+  localStorage.setItem("storedgames", JSON.stringify(storedGames));
+  
+  console.log(storedGames);
+  showScores();
+}
 
 
 
-//startBtn.addEventListener("click", startQuiz);
+function showScores() {
+  form.classList.toggle("hide");
+  scoresArea.classList.toggle("hide");
+  var storedGames = JSON.parse(localStorage.getItem("storedgames"))||[];
+  storedGames.sort(function(a,b){
+    return b.number - a.number
+  });
 
-//code quiz needs a start button
-//needs a timer
-//needs questions
-//when a question is answered incorrectly time is subtrackted from the clock
-// after all questions answered or time hits zero it is game over
-//need to figure out how to save initials and score
+  for(var i=0; i < storedGames.length; i++) {
+ 
+      var createLi = document.createElement("li");
+      createLi.textContent = storedGames[i].player + " " + storedGames[i].number;
+      scoreList.appendChild(createLi);
+  
+  }
+}
+
+reloadBtn.addEventListener("click", reload);
+
+function reload() {
+  location.reload();
+}
